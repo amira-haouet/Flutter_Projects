3e29ef313439fd2f088fd062addb877ebcb4e217
amira-haouet <haouetamira@gmail.com>
2021-12-05 13:59:00 +0100

    update apps

--- manipulateflutterscad/lib/main.dart
+++ manipulateflutterscad/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:manipulateflutterscad/screens/basic_screen.dart';
 void main() => runApp(StaticApp());
 
 class StaticApp extends StatelessWidget {
+    const StaticApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     debugShowCheckedModeBanner: false;
