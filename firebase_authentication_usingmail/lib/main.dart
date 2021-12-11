7559b97a1a530594716a70c8e7ea5006f359566d
amira-haouet <haouetamira@gmail.com>
2021-12-11 14:13:06 +0100

    update flutter app

--- firebase_authentication_usingmail/lib/main.dart
+++ firebase_authentication_usingmail/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
-import 'home.dart';
-import 'signup.dart';
+import 'screens/home.dart';
+import 'screens/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
